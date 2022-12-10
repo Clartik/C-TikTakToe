8a4154e92aa922561fc844f42abb2f3e94190fbf
Heroshrine <skler.mccloud@gmail.com>
2022-12-09 22:54:21 -0800

    Grid.h reference added

--- TikTakToe/Grid.h
+++ TikTakToe/Grid.h
@@ -2,4 +2,4 @@
 
 #pragma once
 void SetGridLocation(int, char); // lets AI (or people) set a grid location
-void UpdateGridLocation(int, char, std::array<std::array<std::array<int, 3>, 3>, 3>); // updates the grid location for the AI to see
+void UpdateGridLocation(int, char, std::array<std::array<std::array<int, 3>, 3>, 3>&); // updates the grid location for the AI to see
