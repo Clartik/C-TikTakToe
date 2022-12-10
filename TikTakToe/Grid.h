7641b8dfd5b8ef7a560969a698d37d974bb6857f
Heroshrine <skler.mccloud@gmail.com>
2022-12-09 23:16:37 -0800

    ??

--- TikTakToe/Grid.h
+++ TikTakToe/Grid.h
@@ -8,5 +8,5 @@ struct GridPoint
 };
 
 #pragma once
-void SetGridLocation(const GridPoint&, const char); // lets AI (or people) set a grid location
+void SetGridLocation(const GridPoint&); // lets AI (or people) set a grid location
 void UpdateGridLocation(const GridPoint&, const char, std::array<std::array<char, 3>, 3>&); // updates the grid location for the AI to see
