03913071f38134c970f4a565b4d54a3ac684549a
Heroshrine <skler.mccloud@gmail.com>
2022-12-09 22:55:32 -0800

    Grid.h const

--- TikTakToe/Grid.h
+++ TikTakToe/Grid.h
@@ -1,5 +1,5 @@
 #include <array>
 
 #pragma once
-void SetGridLocation(int, char); // lets AI (or people) set a grid location
-void UpdateGridLocation(int, char, std::array<std::array<std::array<int, 3>, 3>, 3>&); // updates the grid location for the AI to see
+void SetGridLocation(const int, const char); // lets AI (or people) set a grid location
+void UpdateGridLocation(const int, const char, std::array<std::array<std::array<int, 3>, 3>, 3>&); // updates the grid location for the AI to see
