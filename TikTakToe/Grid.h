8d91f0bb219235311330c06a81e17caa10584145
Heroshrine <skler.mccloud@gmail.com>
2022-12-09 22:58:16 -0800

    Gridpoint to Grid.h

--- TikTakToe/Grid.h
+++ TikTakToe/Grid.h
@@ -1,5 +1,11 @@
 #include <array>
 
+struct GridPoint
+{
+	char x;
+	char y;
+};
+
 #pragma once
-void SetGridLocation(const int, const char); // lets AI (or people) set a grid location
-void UpdateGridLocation(const int, const char, std::array<std::array<std::array<int, 3>, 3>, 3>&); // updates the grid location for the AI to see
+void SetGridLocation(const GridPoint&, const char); // lets AI (or people) set a grid location
+void UpdateGridLocation(const GridPoint&, const char, std::array<std::array<std::array<int, 3>, 3>, 3>&); // updates the grid location for the AI to see
--- TikTakToe/Opponent.cpp
+++ TikTakToe/Opponent.cpp
@@ -1,5 +1,7 @@
 #include "Grid.h"
 
-void UpdateGridLocation(int index, char c) {
+void UpdateGridLocation(const int index, const char c, std::array<std::array<std::array<int, 3>, 3>, 3>& grid) {
+
+
 
 }
