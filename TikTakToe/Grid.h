94332c74643fab0c402b71bc804b1121e61a3cfd
Heroshrine <skler.mccloud@gmail.com>
2022-12-09 22:53:15 -0800

    Grid.h new parameter

--- TikTakToe/Grid.h
+++ TikTakToe/Grid.h
@@ -1,3 +1,5 @@
+#include <array>
+
 #pragma once
 void SetGridLocation(int, char); // lets AI (or people) set a grid location
-void UpdateGridLocation(int, char); // updates the grid location for the AI to see
+void UpdateGridLocation(int, char, std::array<std::array<std::array<int, 3>, 3>, 3>); // updates the grid location for the AI to see
